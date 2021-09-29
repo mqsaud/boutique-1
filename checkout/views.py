b92e208070deaef558b9563fc719734424d4683f
mqsaud <mqsaud@gmail.com>
2021-09-29 20:41:45 +0000

    Add checkout form data caching i payment in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,24 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_Key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -51,7 +70,7 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    mesages.error(request, (
+                    messages.error(request, (
                         "One of the product in your bag wasn't found in our database."
                         "Please call us for assistance")
                     )
@@ -60,7 +79,7 @@ def checkout(request):
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            message.error(request, 'There was an error with your form. \
+            messages.error(request, 'There was an error with your form. \
             Please double check your information')
     else:
         bag = request.session.get('bag', {})
@@ -102,12 +121,12 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed!\
         Your order number is {order_number}. A confermation \
         email will be sent to {order.email}. ')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
-    context ={
+    context = {
         'order': order,
     }
 
