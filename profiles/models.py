028e01fe3edc4321f436470e6d444b4e6d670580
mqsaud <mqsaud@gmail.com>
2021-10-06 11:11:24 +0000

    Update allauth templates and test profile

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    dTruelt_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
@@ -20,7 +20,7 @@ class UserProfile(models.Model):
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
 
-    def __srt__(self):
+    def __str__(self):
         return self.user.username
 
 
