b6c18c33339aae5f79a051aee6d03a3788f38593
mqsaud <mqsaud@gmail.com>
2021-10-08 20:48:22 +0000

    Product Admin - add product view, url and template

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_frindly_name(self):
+    def get_friendly_name(self):
         return self.friendly_name
 
 
